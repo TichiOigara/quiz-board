fc561f56623c66eb74fe8a6f39b9c0228c679822
TichiOigara <sheilanyatichi@gmail.com>
2019-02-22 16:38:14 +0300

    add function in js/scripts.js for adding total score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
+//function for adding total scores.
 var addResults = function(results){
-
+  var totalScore = 0;
+  for(var i = 0; i<results.length; i++){
+    totalScore += results[i];
+  }
+  return totalScore;
 };
 
 $(document).ready(function(){
   $("#form form").submit(function(event){
     event.preventDefault();
+    //array for storing results
     var results =[];
-
-    for(var index =0; index<2; index++){
+    // for loop for entering values from the form to the array
+    for(var index =0; index<10; index++){
       results[index]= parseInt($("input:radio[name=question"+(index+1)+"]:checked").val());
     }
-    // var question1 = parseInt($("input:radio[name=question1]:checked").val());
-    // var question2 = parseInt($("input:radio[name=question2]:checked").val());
-     alert(results);
-    // alert(question2);
+
+     var totalScore = addResults(results);
   });
 
 });
